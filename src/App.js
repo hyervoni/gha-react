0431b6aee30018974bca81dcdcddc665f4f8a6f3
hyervoni <hernanyervoni@gmail.com>
2023-11-08 12:05:46 -0300

    test 6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>React App using Github Actions!!! Esto cambia? y por que pasa?</code>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
